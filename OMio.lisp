5dffc59162c1249fbb2309485d30b4b6a3b899ee
Jose Henrique Padovani <padovani@nics.unicamp.br>
2019-09-19 09:16:12 -0300

    v. 0.11 - Added rowsastrings and colsasstrings features to csv-floats-in.

--- OMio.lisp
+++ OMio.lisp
@@ -1,39 +1,50 @@
-;; OMio - 0.1 - OM input/output: designed to make easier to import/export data
+;; OMio - 0.11 - OM input/output: designed to make easier to import/export data
 ;; 14/sept/2019
 
-
 (defvar omio)
-
 (defpackage omio)
 
 (in-package :omio)
 
+
 (mapc 'om::compile&load (list
+                         ;(make-pathname  :directory (append (pathname-directory *load-pathname*) (list "sources")) :name "xmls" :type "lisp")
                          (make-pathname  :directory (append (pathname-directory *load-pathname*) (list "sources")) :name "omiocsv" :type "lisp")
                          ))
+
+
 
 ;; menu/sub-menu structure
 ;("sub-pack-name" subpacke-lists class-list function-list class-alias-list)
 
+; (defvar omio)
+;
+; (defpackage omio)
 
-(defvar *subpackages-omio-var* nil)
-(setf *subpackages-omio-var*
-         '(("csv" nil nil (
+(defvar *subpackages-list* nil)
+(setf *subpackages-list*
+      '( ("csv" nil nil (csv-floats-in) nil)
+      ;("csv" nil nil (csv-floats-in csv-floats-in2) nil)
+         ))
 
-                           csv-floats-in
 
 
-                                     ) nil)))
+
+
 
    ;--------------------------------------------------
    ;filling packages
    ;--------------------------------------------------
-(om::fill-library *subpackages-omio-var*)
+
+(om::fill-library *subpackages-list*)
 
 
-(om::set-lib-release 0.1)
+(om::set-lib-release 0.11)
 
-(om::fill-library
- '(("csv" nil nil
-    (om::csv-floats-in) nil)
-   ))
+   (print "
+   ;;;-----------------
+   ;;; OMio 0.1 - in/out functions for OpenMusic
+   ;;;
+   ;;; José Henrique Padovani (2019)
+   ;;;===========================================================================
+   ")
--- sources/omiocsv.lisp
+++ sources/omiocsv.lisp
@@ -3,15 +3,49 @@
 
 ; Functions definition
 
-(om::defmethod! csv-floats-in ((filename pathname) &key (defaultformat 'double-float))
-  :initvals '(nil double-float)
-  :indoc '("<n>")
-  :icon 128
-  :doc "reads csv file at \"filename\" and returns a list of lists of floats (that corresponds to the rows of the csv file)"
-  (with-open-file (stream filename)
-                  (loop for line = (read-line stream nil)
-                       while line
-                       collect (loop for start = 0 then (1+ finish)
-                                 for finish = (position #\, line :start start)
-                                 collecting (parse-float (string-trim " " (subseq line start finish)) :default-format defaultformat)
-                                 until (null finish)))))
+; (om::defmethod! csv-floats-in ((filename pathname) &key (defaultformat 'long-float))
+; :initvals '(nil long-float)
+; :indoc '("<n>")
+; :icon 128
+; :doc
+; "reads csv file at \"filename\" and returns a list of lists of floats (that corresponds to therows of the csv file)\n\nKeyword :defaultformat can be either long-float/double-float (default) or float/short-float/single-float."
+; (with-open-file (stream filename)
+; (loop for line = (read-line stream nil)
+; for linecount from 0
+; while line
+; collect (loop for start = 0 then (1+ finish)
+; for finish = (position #\, line :start start)
+; for columncount from 0
+; collecting (parse-float (string-trim " " (subseq line start finish)) :default-format defaultformat)
+; until (null finish)))))
+
+
+(om::defmethod! csv-floats-in ((filename pathname) &optional rowsasstrings colsasstrings &key (defaultformat 'long-float))
+                :initvals '(nil nil nil long-float)
+:indoc '("<n>")
+:icon 128
+:doc
+"reads csv file at \"filename\" and returns a list of lists of floats (that corresponds to therows of the csv file)\n\n
+Optional parameters rowsasstrings and colsasstrings can be nil or lists: they can be used to specify rows or columns of the original csv file that should not be read as float numbers.\n\n
+Keyword :defaultformat can be either long-float/double-float (default) or float/short-float/single-float."
+(with-open-file (stream filename)
+               (loop for line = (read-line stream nil)
+                 while line
+                 for linecount from 0
+                 collect
+                 (loop for start = 0 then (1+ finish)
+                       for columnnumber from 0
+                       for finish = (position #\, line :start start)
+                       collect
+                       (cond
+                        (colsasstrings ;;se colunas forem especificadas
+                         (if rowsasstrings  ;; primeiro ve se é para printar toda a linha como string
+                           (if (position linecount rowsasstrings)(string-trim " " (subseq line start finish)) ;;linha como string
+                             (if (position columnnumber colsasstrings)(string-trim " " (subseq line start finish))(parse-float (string-trim " " (subseq line start finish)) :default-format defaultformat));; senão, ve se a coluna atual deve ser lida como string ou como numero
+                           )
+(if (position columnnumber colsasstrings)(string-trim " " (subseq line start finish))(parse-float (string-trim " " (subseq line start finish)) :default-format defaultformat))
+)
+                         )
+                        (rowsasstrings (if (position linecount rowsasstrings)(string-trim " " (subseq line start finish))(parse-float (string-trim " " (subseq line start finish)) :default-format defaultformat)))
+                        (t (parse-float (string-trim " " (subseq line start finish)) :default-format defaultformat)))
+                       until (null finish)))))
